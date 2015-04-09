1af3754b704877e411d24d753938d59c30fbcfdc
l_r_nightmare <l_r_nightmare@1596915d-9e13-0410-ab7f-96e59408e0f0>
2015-04-09 19:23:05 +0000

    Update FSF address and add the word 'Copyright'
    git-svn-id: https://svn.code.sf.net/p/plibc/code/trunk/plibc@157 1596915d-9e13-0410-ab7f-96e59408e0f0

--- src/include/plibc.h
+++ src/include/plibc.h
@@ -1,6 +1,6 @@
 /*
      This file is part of PlibC.
-     (C) 2005, 2006, 2007, 2008, 2009, 2010 Nils Durner (and other contributing authors)
+     Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010 Nils Durner (and other contributing authors)
 
 	   This library is free software; you can redistribute it and/or
 	   modify it under the terms of the GNU Lesser General Public
@@ -14,7 +14,7 @@
 
 	   You should have received a copy of the GNU Lesser General Public
 	   License along with this library; if not, write to the Free Software
-	   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+	   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 /**
